4a203d6f8da4eccc0c3d2aa1f532fe8b4baf2c8f
LeoLe233 <72309828+LeoLe233@users.noreply.github.com>
2025-09-24 14:49:59 -0400

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.Orchestra;
+
+public class ExampleSubsystem extends SubsystemBase {
+  private final TalonFX m_talon;
+  private final Orchestra m_orchestra;
+
+  /** Creates a new ExampleSubsystem. */
+  public ExampleSubsystem() {
+    m_talon = new TalonFX(0);
+    m_orchestra = new Orchestra();
+    // Add a single device to the orchestra
+    m_orchestra.addInstrument(new TalonFX(0));
+    var status = m_orchestra.loadMusic("ymca.chrp");
+    if (!status.isOK()) {
+      System.out.println("Failed to load music: " + status);
+    }
+    else{
+      m_orchestra.play();
+    }
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
